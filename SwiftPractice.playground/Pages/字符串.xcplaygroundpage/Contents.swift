9bfac96b85cbc97acbdf0241b5e7b3e4636b5e77
dushihao <15093353278@163.com>
2021-03-09 17:38:52 +0800

    字符串 索引功能

--- SwiftPractice.playground/Pages/字符串.xcplaygroundpage/Contents.swift
+++ SwiftPractice.playground/Pages/字符串.xcplaygroundpage/Contents.swift
@@ -8,11 +8,26 @@ let oneCount = oneEmoji.count // 1
 
 // 双向索引，而非随机访问
 extension String {
-    var allPrefixes: [Substring] {
+    // 复杂度 O(n^2)
+    var allPrefixes1: [Substring] {
         return (1...self.count).map(self.prefix)
+    }
+    
+    // 复杂度 O(n)
+    var allPrefixes2: [Substring] {
+        return self.indices.map { self[...$0] }
     }
 }
 
 let hello = "hello"
-hello.allPrefixes // ["h", "he", "hel", "hell", "hello"]
+hello.allPrefixes1 // ["h", "he", "hel", "hell", "hello"]
+hello.allPrefixes2 // ["h", "he", "hel", "hell", "hello"]
+
+//  https://swiftdoc.org/v5.1/protocol/collection/
+/// 遍历一个集合
+for i in hello.indices {
+    print(hello[i])
+}
+
+//
 //: [Next](@next)
